a39e585db0343e19f57e60834023eec4729bc239
Yuichi Shiraishi <friend1ws@gmail.com>
2015-05-10 05:07:09 +0900

    membership plot

--- server.R
+++ server.R
@@ -9,39 +9,63 @@ library(IRanges)
 library(XVector)
 library(Biostrings)
 # library(BSgenome)
-# library(BSgenome.Hsapiens.UCSC.hg19)
-# library(pmsignature)
+library(BSgenome.Hsapiens.UCSC.hg19)
+library(pmsignature)
 
 shinyServer(function(input, output) {
-  output$signature <- renderPlot({
-    
-    # input$file1 will be NULL initially. After the user selects
-    # and uploads a file, it will be a data frame with 'name',
-    # 'size', 'type', and 'datapath' columns. The 'datapath'
-    # column will contain the local filenames where the data can
-    # be found.
+  
+  estimationResult <- reactive({
+    # Take a dependency on input$goButton
     input$goButton
-    
+       
+    # Use isolate() to avoid dependencies
     inFile <- isolate(input$mutFile)
     K <- isolate(input$sigNum)
-    
     if (is.null(inFile))
       return(NULL)
 
+    print(inFile);
+    
     withProgress(message = 'Reading and formating input file', value = 0.2, {
       Sys.sleep(0.1)
-      G <- readMPFile(inFile$datapath, 5)
+      G <- readMPFile(inFile$datapath, numBases = 5)
       incProgress(0.4, message = 'Estimating mutation signatures')
       Sys.sleep(0.1)
       Param <- getPMSignature(G, K, numInit = 1)
     })
     
-
+    return(list(G, Param))
+  })
+  
+  
+  output$signature <- renderPlot({
+    
+    scaleOrNot <- isolate(input$scale)
+    res <- estimationResult();
+    if (is.null(res)) {
+      return();
+    } 
+    
+    res <- estimationResult();
+    Param <- res[[2]];
     par(mfrow = c(2, 3))
     for (i in 1:K) {
-      visPMSignature(Param, i)
+      visPMSignature(Param, i, isScale = scaleOrNot)
     }
+  })
+  
+  output$membership <- renderPlot({
     
+    res <- estimationResult();
+    if (is.null(res)) {
+      return();
+    } 
+    
+    res <- estimationResult();
+    G <- res[[1]];
+    Param <- res[[2]];
+    visMembership(G, Param);
     
   })
+  
 })
--- ui.R
+++ ui.R
@@ -3,17 +3,22 @@ library(shiny)
 shinyUI(fluidPage(
   title = "pmsignture for shiny",
   
-  titlePanel("pmsignature"),
+  titlePanel("pmsignature"), 
   
-  plotOutput('signature'),
+  sidebarLayout(
   
-  hr(),
-  
-  fluidRow(
-    mainPanel(
+    sidebarPanel(
       fileInput('mutFile', "Update the input file (only mutation position format)"),
       numericInput("sigNum", label = "#signature", value = 3, min = 2, max = 6),
-      actionButton("goButton", "Execute!")
+      checkboxInput("scale", label = "scaling flanking bases", value = FALSE),
+      actionButton("goButton", label = "Execute!")
+    ),
+  
+    mainPanel(
+      plotOutput('signature'),
+      plotOutput('membership')
     )
+    
   )
+  
 ))
